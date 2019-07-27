4403497e38ef7ffdecc638bbd1c1179692524510
David E Lares S <david.e.lares@gmail.com>
2019-07-27 11:35:08 -0400

    Blockchain stats, behaviour and genesis hash

---
+++ src/com/davidlares/davidchain/Stats.java
@@ -0,0 +1,10 @@
+package com.davidlares.davidchain;
+
+public class Stats {
+	// leading zeros
+	public static final int DIFFICULTY = 1;
+	// miner reward
+	public static final double REWARD = 10;
+	// first block = Genesis hash
+	public static final String GENESIS = "0000000000000000000000000000000000000000000000000000000000000000";
+}
