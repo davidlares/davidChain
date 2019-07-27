a0f5d95ea3e1445127066e5d1e9d8945322c7ad2
David E Lares S <david.e.lares@gmail.com>
2019-07-27 12:41:55 -0400

    Testing block instance

--- src/com/davidlares/davidchain/Main.java
+++ src/com/davidlares/davidchain/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Blockchain - davidChain");
+		// Creating a block
+		Block genesis = new Block(0, "Genesis transaction", Stats.GENESIS);
 	}
 
 }
