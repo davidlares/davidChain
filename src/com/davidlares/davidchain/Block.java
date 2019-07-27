c1ee0f582392f05a316c29eb9b494c6cdc1f6b80
David E Lares S <david.e.lares@gmail.com>
2019-07-27 11:58:48 -0400

    prototyping Block structure without SHA256 hashing process

---
+++ src/com/davidlares/davidchain/Block.java
@@ -0,0 +1,57 @@
+package com.davidlares.davidchain;
+import java.util.Date;
+
+//Block structure for blockchain.
+
+public class Block {
+	
+	private int id;
+	// number only used once - used for calculate and generate hashes
+	private int nonce;
+	// this will be used for registering the block time creation
+	private long timeStamp;
+	// hash structure
+	private String hash;
+	private String prevHash;
+	private String transaction;
+	
+	// constructor 
+	public Block(int id, String transaction, String prevHash) {
+		this.id = id;
+		this.transaction = transaction;
+		this.prevHash = prevHash;
+		// setting a unique time creation for the Block
+		this.timeStamp = new Date().getTime();
+		generateHash(); // generating block hash for mining
+	}
+	
+	// getters and setters
+	
+	public String getHash() {
+		return this.hash;
+	}
+	
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+	
+	public String getPrevHash() {
+		return this.prevHash;
+	}
+	
+	public void setPrevHash(String prevHash) { 
+		this.prevHash = prevHash;
+	}
+	
+	// custom methods
+	
+	public void incrementNonce() {
+		this.nonce ++; // incremented when miner tries to find the goldenHash
+	}
+	
+	public void generateHash() {
+		String dataToHash = Integer.toString(id) + prevHash + Long.toString(timeStamp) + Integer.toString(nonce) + transaction.toString();
+		// this dataToHash should be "encrypted" by SHA256
+		this.hash = dataToHash; // this will change
+	}
+}
